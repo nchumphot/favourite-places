efa4ae295ee4c5ce2c5a5788296fc08797c2d07f
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-10-25 12:11:25 +0100

    Used map method for places

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,37 +1,41 @@
-import PlaceEntry from "./PlaceEntry";
+// import PlaceEntry, { placetoElement } from "./PlaceEntry";
+import { placetoElement } from "./PlaceEntry";
 
 function MainContent(): JSX.Element {
   return (
-    <>
-      <PlaceEntry
-        title={"Two-in-One"}
-        location={{ name: "Mosque-Cathedral of Córdoba", country: "Spain" }}
-        image={"./img/cordoba.jpg"}
-        mapLink={"https://goo.gl/maps/NceZmx1ti4eVDoqh7"}
-        caption={"Insert caption here"}
-      />
-      <PlaceEntry
-        title={"Forever Summertime"}
-        location={{ name: "Phuket", country: "Thailand" }}
-        image={"./img/phuket.jpg"}
-        mapLink={"https://goo.gl/maps/7UTxstsfDG6UwSNGA"}
-        caption={"Insert caption here"}
-      />
-      <PlaceEntry
-        title={"Capital of the World"}
-        location={{ name: "London", country: "England" }}
-        image={"./img/london.jpg"}
-        mapLink={"https://goo.gl/maps/gNA4bjbJGAQud1hf7"}
-        caption={"Insert caption here"}
-      />
-      <PlaceEntry
-        title={"The Real 21st Century"}
-        location={{ name: "Tokyo", country: "Japan" }}
-        image={"./img/tokyo.jpg"}
-        mapLink={"https://goo.gl/maps/wGuu4Qow4tYvFJ1N6"}
-        caption={"Insert caption here"}
-      />
-    </>
+    <div>
+      {placetoElement}
+    </div>
+    // <>
+    //   {/* <PlaceEntry
+    //     title={"Two-in-One"}
+    //     location={{ name: "Mosque-Cathedral of Córdoba", country: "Spain" }}
+    //     image={"./img/cordoba.jpg"}
+    //     mapLink={"https://goo.gl/maps/NceZmx1ti4eVDoqh7"}
+    //     caption={"Insert caption here"}
+    //   />
+    //   <PlaceEntry
+    //     title={"Forever Summertime"}
+    //     location={{ name: "Phuket", country: "Thailand" }}
+    //     image={"./img/phuket.jpg"}
+    //     mapLink={"https://goo.gl/maps/7UTxstsfDG6UwSNGA"}
+    //     caption={"Insert caption here"}
+    //   />
+    //   <PlaceEntry
+    //     title={"Capital of the World"}
+    //     location={{ name: "London", country: "England" }}
+    //     image={"./img/london.jpg"}
+    //     mapLink={"https://goo.gl/maps/gNA4bjbJGAQud1hf7"}
+    //     caption={"Insert caption here"}
+    //   />
+    //   <PlaceEntry
+    //     title={"The Real 21st Century"}
+    //     location={{ name: "Tokyo", country: "Japan" }}
+    //     image={"./img/tokyo.jpg"}
+    //     mapLink={"https://goo.gl/maps/wGuu4Qow4tYvFJ1N6"}
+    //     caption={"Insert caption here"}
+    //   /> */}
+    // </>
   );
 }
 
